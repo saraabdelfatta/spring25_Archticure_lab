de906d4b95373d8c919963ab6276b3cb6841a20d
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-02-23 22:27:51 +0200

    Update mux.v

--- Lab_3/Lab_3/exp2/files/mux.v
+++ Lab_3/Lab_3/exp2/files/mux.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2025 01:25:37 PM
-// Design Name: 
-// Module Name: mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module mux(input a, input b, input sel, output out
