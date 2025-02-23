8fae529f9111906be8d043de741585234835c82c
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-02-23 21:09:04 +0200

    Add files via upload

---
+++ Lab_3/Lab_3/exp1/files/N-bit _eg.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/18/2025 12:09:13 PM
+// Design Name: 
+// Module Name: N-bit _reg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module N-bit _reg(
+
+    );
+endmodule
