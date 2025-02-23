a3a994596456ba7eef486db2d4002080152303ac
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-02-23 23:04:34 +0200

    Update nbit_shift_left.v

--- Lab_3/Lab_3/exp3/files/nbit_shift_left.v
+++ Lab_3/Lab_3/exp3/files/nbit_shift_left.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2025 01:51:50 PM
-// Design Name: 
-// Module Name: nbit_shift_left
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module nbit_shift_left#(parameter n = 8)(input [n-1:0] in, output [n-1:0] out
