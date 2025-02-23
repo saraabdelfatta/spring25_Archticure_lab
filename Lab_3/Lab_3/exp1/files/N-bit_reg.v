a17de3e88c6d4e0402cd7abcd63543634bc19161
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-02-23 21:44:26 +0200

    Update N-bit_reg.v

--- Lab_3/Lab_3/exp1/files/N-bit_reg.v
+++ Lab_3/Lab_3/exp1/files/N-bit_reg.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2025 12:09:57 PM
-// Design Name: 
-// Module Name: N-bit_reg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module N_bit_reg#(parameter n = 8)(input clk, input rst, input load, input [n-1 : 0] D, output [n-1 : 0] Q 
