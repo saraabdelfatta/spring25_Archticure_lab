46866128dd2e76f0024309ce74b71957f911437a
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-03-02 17:34:38 +0200

    Update Full_Adder.v

--- lab 4/lab files/exp1/files/Full_Adder.v
+++ lab 4/lab files/exp1/files/Full_Adder.v
@@ -1,7 +1,8 @@
 `timescale 1ns / 1ps
 
 
-module Full_Adder(input a, input b, input cin, output sum, output cout);
+module Full_Adder(input a, input b, input cin,
+        output sum, output cout);
 
 assign {cout, sum} = a + b + cin;
 
