7930a630696383091f20841128177894ea4873af
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-03-02 17:51:53 +0200

    Update ALU.v

--- lab 4/lab files/exp1/files/ALU.v
+++ lab 4/lab files/exp1/files/ALU.v
@@ -1,4 +1,6 @@
 `timescale 1ns / 1ps
+
+
 module ALU#(n = 32)(
     input [n-1:0] a,
     input [n-1:0] b,
@@ -6,10 +8,12 @@ module ALU#(n = 32)(
     output reg zeroFlag,
     output reg [n:0] ALU_out
     );
+    
     wire [n-1:0] sum;
     wire [n-1:0] b_twos;
     assign b_twos = (sel[3:0] == 4'b0010) ? b : ~b;
     Ripple_Carry_Adder #(.n(n)) RCA (.a(a), .b(b_twos), .cin(sel[2]), .sum(sum));
+    
     always@(*)
     begin
         case(sel)
@@ -19,6 +23,7 @@ module ALU#(n = 32)(
             4'b0001: ALU_out = a | b;
             default: ALU_out = 0;
         endcase
+        
         if(ALU_out != 0)
             zeroFlag = 0;
         else
