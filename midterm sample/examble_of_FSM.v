fbc10f36e04e1339e5e955976b4ce8ec2f8a407b
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-04-08 01:34:58 +0200

    Update examble_of_FSM.v

--- midterm sample/examble_of_FSM.v
+++ midterm sample/examble_of_FSM.v
@@ -1,28 +1,35 @@
-Code Example 1: Moore FSM to detect two successive ones (1’s) 
-module fsm(input clk, rst, w, output z); 
-reg [1:0] state, nextState; 
-parameter [1:0] A=2’b00, B=2’b01, C=2’b10; // States Encoding 
+//Code Example 1: Moore FSM to detect two successive ones (1’s) 
+//the need tools for this experments is clck, rest, input and output
+module fsm(input clk, rst, w, output z);  
+  //w is the inpt
+  //z is the output
+
+  //are to intilize the current and next state
+  reg [1:0] state, nextState;  
+
+  //are the parameter of the question
+  //is differ from one question to another
+  //start from here is the core of the question
+  parameter [1:0] A=2’b00, B=2’b01, C=2’b10; // States Encoding  
+
 // Next state generation (combinational logic) 
-always @ (w or state) 
-case (state) 
-A: if (w==0) 
-nextState = A; 
-else   
-B: if (w==0)  
-else   
-C: if (w==0)  
-else   
-default:   
-endcase 
-// State register 
-else  
-end 
-nextState = B; 
-nextState = A; 
-nextState = C; 
-nextState = A; 
-nextState = C; 
-nextState = A; 
+  //in this block we depend on the input and the current state
+  always @(w or state)
+    case(state)
+      A:if(w==0)
+        nextState = A;
+      else
+        nextState = B;
+      B:if(w==0)
+        nextState = A;
+      else
+        nextState = C;
+      C:if(w==0)
+        nextState = A;
+      else
+        nextState = C;
+      default: nextState = A;
+      endcase 
 // Update state FF’s with the triggering edge of the clock 
 always @ (posedge clk or posedge rst) begin 
 if(rst) 
