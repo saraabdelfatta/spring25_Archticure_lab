e2bd9adc49e5d354e20c939bc95d3cca9f8a1483
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-04-08 01:47:30 +0200

    Update clockDivider.v

--- midterm sample/clockDivider.v
+++ midterm sample/clockDivider.v
@@ -1,12 +1,18 @@
 `timescale 1ns / 1ps
 
+//n is the parameter that define the divided ratio
 module clockDivider #(parameter n=5000000)(
-    input clk,
-    input rst,
-    input en,
-    output reg out_clk
+    input clk,  //clk
+    input rst, //rest
+    input en, //enable
+    output reg out_clk  //(divided clock, declared as a register)
     );
+
+    //Calculates the minimum number of bits needed to count up to n-1 using the system function 
+    //$clog2 (ceiling of log base 2).
 parameter Width=$clog2(n);
+
+    //Declares a counter register with width calculated above to count clock cycles.
 reg [Width-1:0] count;
 
 always @(posedge clk, posedge rst) begin
