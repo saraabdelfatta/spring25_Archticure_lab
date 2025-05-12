2a4a6cb4e5d996c941d83053009fc27963889ac5
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-05-12 19:30:05 +0300

    Update MUX_2x1.v

--- lab 3/labs files/exp1/files/MUX_2x1.v
+++ lab 3/labs files/exp1/files/MUX_2x1.v
@@ -1,29 +1,12 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/18/2025 12:03:27 PM
-// Design Name: 
-// Module Name: MUX_2x1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module MUX_2x1(input a, input b, input load, output out
 
-    );
+module MUX_2x1(input a, input b, input load, output out);
     
     assign out = (load == 1'b1) ? a : b;
+
+//or 
+    //assign out = ((a &load ) | (b = ~load));
     
 endmodule
