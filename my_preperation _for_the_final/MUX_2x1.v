3d0fc037972c188ea0d9f718300ddc35aad7d1bb
saraabdelfatta <112962589+saraabdelfatta@users.noreply.github.com>
2025-05-12 22:05:44 +0300

    Add files via upload

---
+++ my_preperation _for_the_final/MUX_2x1.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+
+
+module MUX_2x1(input a, input b, input load, output out);
+    
+    assign out = (load == 1'b1) ? a : b;
+
+//or 
+    //assign out = ((a &load ) | (b = ~load));
+    
+endmodule
